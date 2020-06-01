8984d13e92b10003807fda524c460b47f6893c59
Oracle Public Cloud User <opc@instance-20200531-2236.subnet.vcn.oraclevcn.com>
2020-06-01 05:40:27 +0000

    Updates

---
+++ precache-manifest.54beec502cae04fadf74ae0e8c8185ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18261f1f7024bbdec2b31137306cf93",
+    "url": "/movieql-client/index.html"
+  },
+  {
+    "revision": "ae85c9e46041260deef2",
+    "url": "/movieql-client/static/js/2.30861721.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/movieql-client/static/js/2.30861721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0174ed18fcea74583f5",
+    "url": "/movieql-client/static/js/main.bac473ac.chunk.js"
+  },
+  {
+    "revision": "ccbf866d7b8fe76c948c",
+    "url": "/movieql-client/static/js/runtime-main.dc2cadfe.js"
+  }
+]);
